f7c465be85ac59ea6774363245226ac5e766a9a0
Jeff Tucker <jedatu@gmail.com>
2019-03-25 21:09:13 -0400

    WIP

--- post-message.js
+++ post-message.js
@@ -3,6 +3,6 @@ var scope = "default";
 
 if (location.host != "jedatu.github.io")
 {
-    scope = document.getElementById("message").nodeValue;
+    scope = document.getElementById("message").value;
     setInterval(() => { console.log("TEST:", scope)}, 3000);
 }
