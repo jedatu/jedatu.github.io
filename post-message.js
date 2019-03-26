2935b32ff7e6045a255e3af69a1563fe440dbaf2
Jeff Tucker <jedatu@gmail.com>
2019-03-26 17:07:57 -0400

    WIP

--- post-message.js
+++ post-message.js
@@ -1,28 +1,39 @@
-console.log('script loaded. v7');
+console.log('script loaded. v8');
 window.scope = "default";
 
 if (location.host != "jedatu.github.io")
 {
+    window.addEventListener("message", parentReceiveMessage, false);
+
+    function parentReceiveMessage(event)
+    {
+        if (typeof(event.data) == "string")
+        {
+            document.getElementById("log").innerHTML="IFrame Received: <br/>" + event.data + " from <br/>" + event.origin;
+            console.log("PARENT RECEIEVED", event);
+        }
+    }
+
     setInterval(() => { 
         var new_scope = document.getElementById("message").value;
         if (scope != new_scope)
         {
             scope = new_scope;
-            document.getElementsByTagName("iframe")[0].contentWindow.postMessage(scope, "https://jedatu.github.io");
+            document.getElementsByTagName("iframe")[0].contentWindow.postMessage(scope, "*");
             //scope = document.getElementsByTagName("iframe")[0].contentWindow.contentDocument;
             console.log("PARENT:", scope)
         }
     }, 3000);
 }
 else {
-    window.onmessage
-    window.addEventListener("message", receiveMessage, false);
+    
+    window.addEventListener("message", iframeReceiveMessage, false);
 
-    function receiveMessage(event)
+    function iframeReceiveMessage(event)
     {
         if (typeof(event.data) == "string")
         {
-            document.getElementById("framed").innerHTML="IFrame Received: " + event.data + " from " + event.origin;
+            document.getElementById("framed").innerHTML="IFrame Received: <br/>" + event.data + " from <br/>" + event.origin;
             console.log("FRAME", event);
         }
     }
@@ -32,3 +43,4 @@ else {
     //}, 9000);
 }
 
+//https://hackerone.com/reports/207042
