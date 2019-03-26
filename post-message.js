bedb44b82a580ef977a7a467b8997f378417d623
Jeff Tucker <jedatu@gmail.com>
2019-03-25 20:55:15 -0400

    WIP

--- post-message.js
+++ post-message.js
@@ -1,9 +1,8 @@
-console.log('script loaded. v3');
+console.log('script loaded. v4');
 var scope = "default";
 
 if (location.host != "jedatu.github.io")
 {
     scope = document.getElementById("message").nodeValue;
+    setTimeout(() => { console.log("TEST:", scope)}, 3000);
 }
-
-setTimeout(() => { console.log("TEST:", scope)}, 3000);
