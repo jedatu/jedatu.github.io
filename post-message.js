860f2ff069f432e5b6574e1a9205aa9c928f208a
Jeff Tucker <jedatu@gmail.com>
2019-03-26 14:17:56 -0400

    WIP

--- post-message.js
+++ post-message.js
@@ -4,14 +4,23 @@ window.scope = "default";
 if (location.host != "jedatu.github.io")
 {
     setInterval(() => { 
-        //scope = document.getElementById("message").value;
-        scope = document.getElementsByTagName("iframe")[0].contentWindow.contentDocument;
-        console.log("parent:", scope)
+        scope = document.getElementById("message").value;
+        document.getElementsByTagName("iframe")[0].contentWindow.postMessage(scope, "jedatu.github.io");
+        //scope = document.getElementsByTagName("iframe")[0].contentWindow.contentDocument;
+        console.log("PARENT:", scope)
     }, 3000);
 }
 else {
-    setInterval(() => { 
-        console.log("frame:", scope);
-    }, 9000);
+    window.onmessage
+    window.addEventListener("message", receiveMessage, false);
+
+    function receiveMessage(event)
+    {
+        console.log("FRAME", event);
+    }
+
+    //setInterval(() => { 
+    //    console.log("frame:", scope);
+    //}, 9000);
 }
 
