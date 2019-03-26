9989ea1b74ef5c87eae2b5d78301b42cf9baa104
Jeff Tucker <jedatu@gmail.com>
2019-03-25 22:48:46 -0400

    WIP

--- post-message.js
+++ post-message.js
@@ -1,16 +1,17 @@
 console.log('script loaded. v6');
-var scope = "default";
+window.scope = "default";
 
 if (location.host != "jedatu.github.io")
 {
     setInterval(() => { 
-        scope = document.getElementById("message").value;
+        //scope = document.getElementById("message").value;
+        scope = document.getElementsByTagName("iframe")[0].contentWindow.contentDocument;
         console.log("parent:", scope)
     }, 3000);
 }
 else {
     setInterval(() => { 
         console.log("frame:", scope);
-    }, 3000);
+    }, 9000);
 }
 
