ca55dea785339583cd81e3ff7973ec64cc141cf7
Jeff Tucker <jedatu@gmail.com>
2019-03-27 22:32:17 -0400

    WIP

--- host-script.js
+++ host-script.js
@@ -1,22 +1,24 @@
+window.scope = "Parent";
+
 window.addEventListener("message", parentReceiveMessage, false);
 
-    function parentReceiveMessage(event)
+function parentReceiveMessage(event)
+{
+    if (typeof(event.data) == "string")
     {
-        if (typeof(event.data) == "string")
-        {
-            document.getElementById("log").innerHTML="IFrame Received: <br/>" + event.data + " from <br/>" + event.origin;
-            console.log("PARENT RECEIEVED", event);
-        }
+        document.getElementById("log").innerHTML="IFrame Received: <br/>" + event.data + " from <br/>" + event.origin;
+        console.log("PARENT RECEIEVED", event);
     }
+}
 
-    setInterval(() => { 
-        var new_scope = document.getElementById("message").value;
-        if (scope != new_scope)
-        {
-            scope = new_scope;
-            //document.getElementsByTagName("iframe")[0].contentWindow.postMessage(scope, "*");
-            //scope = document.getElementsByTagName("iframe")[0].contentWindow.contentDocument;
-            document.getElementsByTagName("iframe")[0].contentWindow.frameAction(scope);
-            console.log("PARENT:", scope);
-        }
-    }, 3000);
+setInterval(() => { 
+    var new_scope = document.getElementById("message").value;
+    if (scope != new_scope)
+    {
+        scope = new_scope;
+        //document.getElementsByTagName("iframe")[0].contentWindow.postMessage(scope, "*");
+        //scope = document.getElementsByTagName("iframe")[0].contentWindow.contentDocument;
+        document.getElementsByTagName("iframe")[0].contentWindow.frameAction(scope);
+        console.log("PARENT:", scope);
+    }
+}, 3000);
